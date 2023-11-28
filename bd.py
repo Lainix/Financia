84ae1ce83ddc03347c4668d411bed34516bf251a
Lainix <32379941+Lainix@users.noreply.github.com>
2023-11-27 23:41:03 -0600

    first commit

---
+++ bd.py
@@ -0,0 +1,8 @@
+import pymysql
+
+
+def obtener_conexion():
+    return pymysql.connect(host='b2wg0fcubawvci7kbu35-mysql.services.clever-cloud.com',
+                                user='ustewyxg17xo2qar',
+                                password='tlEYCwdJJc5UAISKx44y',
+                                db='b2wg0fcubawvci7kbu35')
